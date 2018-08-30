654a104980e4d3ed1d50b8573a941f733fa16ce2
dakedroid <dave1998._@hotmail.com>
2018-08-30 12:59:58 -0500

    Example firebase added

---
+++ app/src/main/java/com/divaga/tecnologico/viewmodel/MainActivityViewModel.java
@@ -0,0 +1,37 @@
+package com.divaga.tecnologico.viewmodel;
+
+import android.arch.lifecycle.ViewModel;
+import com.divaga.tecnologico.MainActivity;
+import com.divaga.tecnologico.Filters;
+
+
+/**
+ * ViewModel for {@link MainActivity}.
+ */
+
+public class MainActivityViewModel extends ViewModel {
+
+    private boolean mIsSigningIn;
+    private Filters mFilters;
+
+    public MainActivityViewModel() {
+        mIsSigningIn = false;
+        mFilters = Filters.getDefault();
+    }
+
+    public boolean getIsSigningIn() {
+        return mIsSigningIn;
+    }
+
+    public void setIsSigningIn(boolean mIsSigningIn) {
+        this.mIsSigningIn = mIsSigningIn;
+    }
+
+    public Filters getFilters() {
+        return mFilters;
+    }
+
+    public void setFilters(Filters mFilters) {
+        this.mFilters = mFilters;
+    }
+}
